a163304a44611c84fd4777c6ea039037287719a3
Debulois Quentin <quentin.debulois@gmail.com>
2025-06-07 01:07:03 +0200

    Ajout de phpunit pour des tests qu on ne fera jamais

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,6 +31,18 @@ class ComposerAutoloaderInitd0e7fa23c30875e8491eb12a5e1288b2
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitd0e7fa23c30875e8491eb12a5e1288b2::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
